c319504af165ca788a5bce43a56d05705f8b7580
zinirun <heojj97@naver.com>
2021-01-29 16:02:08 +0900

    Update update-movie.dto.ts

--- src/movies/dto/update-movie.dto.ts
+++ src/movies/dto/update-movie.dto.ts
@@ -1,5 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { IsNumber, IsString } from 'class-validator';
 import { CreateMovieDto } from './create-movie.dto';
 
 export class UpdateMovieDto extends PartialType(CreateMovieDto) {}
